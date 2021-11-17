a4109ef833b73a3fc7a3bb2e155e4727267afd63
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-11-17 23:29:13 +0300

    backend/frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import './bootstrap.min.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="p-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+    
+  );
+}
+
+export default App
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+
+function Header() {
+    return (
+        <header>
+            <Navbar bg="primary" variant="primary" expand="lg">
+                <Container className="d-flex justify-content-spacebetween align-items-center">
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ProShop</Navbar.Brand>
+                    </LinkContainer>             
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <LinkContainer to="/cart">
+                                <Nav.Link><i className="fas fa-shopping-cart me-2"></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link><i className="fas fa-user me-2"></i>Login</Nav.Link>
+                            </LinkContainer>                
+                        </Nav>
+                    </Navbar.Collapse>     
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header
