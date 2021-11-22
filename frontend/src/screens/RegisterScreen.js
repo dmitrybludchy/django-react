e7379384e8b669238ba489f34d4a1584181e2104
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-11-22 13:43:50 +0300

    shipping screen, save address

--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -44,7 +44,7 @@ function RegisterScreen({ location, history }) {
                     <Form.Label>Name</Form.Label>
                     <Form.Control
                         required
-                        type='name'
+                        type='text'
                         placeholder='Enter name'
                         value={name}
                         onChange={(e) => setName(e.target.value)}
