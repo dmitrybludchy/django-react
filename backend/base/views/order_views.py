5541ebd617cd6c1f08d1449a29cc4103a02b032b
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-11-24 23:58:06 +0300

    update to paid endpoint

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -5,6 +5,7 @@ from rest_framework import serializers, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
+from datetime import datetime
 
 
 @api_view(['POST'])
@@ -66,3 +67,13 @@ def getOrderById(request, pk):
             Response({'detail': 'Not authorized to view this order'}, status=status.HTTP_400_BAD_REQUEST)
     except:
         return Response({'detail': 'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
