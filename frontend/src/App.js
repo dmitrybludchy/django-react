14dadcb4bf2a730fbcafcaab9c9c0f5ffc97bf3d
Dmitry Bludchy <dmitrybludchy@gmail.com>
2021-11-27 13:40:55 +0300

    admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import RegisterScreen from './screens/RegisterScreen'
 import ShippingScreen from './screens/ShippingScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
         </Container>
       </main>
       <Footer />
